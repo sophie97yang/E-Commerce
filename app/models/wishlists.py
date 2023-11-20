6d1c4e671e5506368060f72c202ec8c4495a9517
Sophie Yang <sophie97yang@gmail.com>
2023-11-20 10:39:42 -0800

    migration and seeder files edits for deployment

--- app/models/wishlists.py
+++ app/models/wishlists.py
@@ -3,8 +3,8 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 wishlists = db.Table(
     "wishlists",
     db.Model.metadata,
-    db.Column("member_id", db.Integer, db.ForeignKey("members.id"), primary_key=True),
-    db.Column("product_id", db.Integer, db.ForeignKey("products.id"), primary_key=True)
+    db.Column("member_id", db.Integer, db.ForeignKey(add_prefix_for_prod("members.id")), primary_key=True),
+    db.Column("product_id", db.Integer, db.ForeignKey(add_prefix_for_prod("products.id")), primary_key=True)
 )
 
 if environment == "production":
