8de19578bb08e00101389b2d231d97409aaa96b2
Yoseph <yoslatif@gmail.com>
2023-11-20 13:24:31 -0800

    Landing edit

--- react-app/src/components/LandingPage/Landing.js
+++ react-app/src/components/LandingPage/Landing.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carousel styles
 import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carousel styles
+
 
 function LandingPage() {
   const [products, setProducts] = useState([]);
-  const [backgroundImage, setBackgroundImage] = useState('');
   const history = useHistory();
+  const currentUserRole = useSelector((state) => state.session.user?.role); // Get the user role at the top level
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -21,34 +23,21 @@ function LandingPage() {
     fetchProducts();
   }, []);
 
-  useEffect(() => {
-    const imageUrls = [
-      'https://example.com/image1.jpg',
-      'https://example.com/image2.jpg',
-      'https://example.com/image3.jpg',
-    ];
-    const randomIndex = Math.floor(Math.random() * imageUrls.length);
-    setBackgroundImage(imageUrls[randomIndex]);
-  }, []);
 
   const handleBoxClick = (route, userRole) => {
-    const currentUserRole = getCurrentUserRole(); // Replace with actual logic to get the user's role
-  
-    if (userRole === 'all' || currentUserRole === userRole) {
+    if (userRole === "all" || currentUserRole === userRole) {
       history.push(route);
     } else {
-      // Handle unauthorized access, e.g., redirect to a different page or show a message
-      console.log('Unauthorized access');
+      console.log("Unauthorized access");
     }
   };
-  
+
   // Define boxes for rendering
   const boxes = [
-    { title: 'All Products', route: '/products', userRole: 'all' },
-    { title: 'Create Product', route: '/create-product', userRole: 'seller' },
+    { title: "All Products", route: "/products", userRole: "all" },
+    { title: "Create Product", route: "/create-product", userRole: "seller" },
     // Add more boxes
   ];
-  
 
   return (
     <>
@@ -60,16 +49,21 @@ function LandingPage() {
           </div>
         ))}
       </Carousel>
-      <div className='boxes-container'>
+
+      <div className="boxes-container">
         {boxes.map((box, index) => (
-          <div key={index} className='box' onClick={() => handleBoxClick(box.route, box.userRole)}>
+          <div
+            key={index}
+            className="box"
+            onClick={() => handleBoxClick(box.route, box.userRole)}
+          >
             <h3>{box.title}</h3>
-            {/* Add other box content */}
           </div>
         ))}
       </div>
-      {/* Add other components or elements here */}
+      {/* ... rest of your JSX ... */}
     </>
   );
 }
+
 export default LandingPage;
